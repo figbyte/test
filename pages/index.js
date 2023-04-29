d127520dcb43717f59aeb093638b5e8dac3d32c5
figbyte <132148592+figbyte@users.noreply.github.com>
2023-04-29 17:04:31 +0000

    add gif

--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,50 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import { useState } from 'react'
+import Image from 'next/image';
+
 
 export default function Home() {
   const [name, setName] = useState()
 
-
   const handleInput = (e) => {
     setName(e.target.value)
   }
+  const [kata, setKata] = useState()
+
+
+  const handleInputkata = (e) => {
+    setKata(e.target.value)
+  }
 
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <div className="container">
+        <Head>
+          <title>Next.js Starter!</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <Header title="hola!" />
+        <main>
+          <Header title="hola!" />
 
-        <div>
-          Masukan nama anda <input type="text" onChange={handleInput} />
+          <div>
+            Masukan nama anda <input type="text" onChange={handleInput} />
+          </div>
+          <div style={{ marginBottom: "50px" }}>
+            Masukan kata kata <input type="text" onChange={handleInputkata} />
+          </div>
 
-        </div>
 
-        Hello nama saya adalah {name}
+          <div>Hello nama saya adalah <b>{name}</b></div>
+      <div>kata anda <b>{kata}</b></div>
 
-      </main>
+        </main>
+      </div>
+<img src="https://media.tenor.com/cKy0WOEi6pgAAAAC/rotating-banana-meme-rotating-banana.gif" alt="My GIF" width={400} height={300} />
+   
 
-      <Footer />
+
     </div>
   )
 }
